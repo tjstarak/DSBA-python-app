21a91c8675e1d89583e184443a06a45f8f3c6091
tjstarak <tomasz.starakiewicz@gmail.com>
2022-01-25 20:14:25 +0100

    del old, ref update in apt-val

--- apt_valuation.py
+++ apt_valuation.py
@@ -8,6 +8,7 @@ from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import cross_val_score, KFold
+from database import get_clean_values
 
 import pickle
 
